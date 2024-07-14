e757bb129da3ddd7b2e48553e6f8fdf50d200dfb
owbird <owbirdphorccuo@gmail.com>
2024-07-14 12:18:13 +0000

    Update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -9,9 +9,10 @@ set -x FLYCTL_INSTALL ~/.fly
 set -x PATH $FLYCTL_INSTALL/bin $PATH
 set -x PATH ~/Workspace/bin $PATH
 set -x PATH /opt/nvim-linux64/bin $PATH
+set -x PATH ~/.bun/bin $PATH
 
 function fish_greeting
-  	 neofetch
+    neofetch --source ~/.config/fish/ascii.txt
     starship init fish | source
     #alias ls='exa -b --grid --icons -F -H --group-directories-first --git -1'
     alias ls='exa -b  --long --grid --icons --git'
