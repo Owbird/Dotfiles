bc1f1817070786abc8df6c7b37aa2e12c9a4037f
owbird <owbirdphorccuo@gmail.com>
2024-07-28 09:52:16 +0000

    Use workspace.py

---
+++ workspace.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+from os.path import expandvars, join
+from os import walk
+from subprocess import run
+from sys import argv
+
+expandvars("$PATH")
+
+project = argv[1]
+
+ignore = [".git", "node_modules", "env", "venv", ".next"]
+
+
+for root, folders, files in walk("/home/owbird/Workspace/"):
+
+    for folder in folders:
+        if folder in ignore:
+            folders.remove(folder)
+
+        if folder.lower() == project.lower():
+            project_path = join(root, project)
+            run(["kitty", "@", "launch", "--type=tab", "--cwd",
+                project_path, "--title", project, "nvim", "."])
+            run(["kitty", "@", "resize-window", "-a", "vertical", "-i", "8"])
+            run(["kitty", "@", "launch", "--type=window",
+                "--cwd", project_path, "--title", project])
+            run(["kitty", "@", "launch", "--type=window",
+                "--cwd", project_path, "--title", project])
--- workspace.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo /home/owbird/Workspace/$1/$2
-kitty @ launch --type=tab --cwd=/home/owbird/Workspace/$1/$2 --title=$2 nvim .
-kitty @ resize-window -a vertical -i 8
-kitty @ launch --type=window --cwd=/home/owbird/Workspace/$1/$2 --title=$2
-kitty @ launch --type=window --cwd=/home/owbird/Workspace/$1/$2 --title=$2
-
