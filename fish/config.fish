b7a31938eb669d01f587a82aedb1379599fa585a
owbird <owbirdphorccuo@gmail.com>
2024-10-02 00:14:19 +0000

    use fastfetch

--- fish/config.fish
+++ fish/config.fish
@@ -12,7 +12,7 @@ set -x PATH /opt/nvim-linux64/bin $PATH
 set -x PATH ~/.bun/bin $PATH
 
 function fish_greeting
-    neofetch --source ~/.config/fish/ascii.txt
+    fastfetch --logo ~/.config/fish/ascii.txt
     starship init fish | source
     #alias ls='exa -b --grid --icons -F -H --group-directories-first --git -1'
     alias ls='exa -b  --long --grid --icons --git'
