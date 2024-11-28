f4a951623a33f0ec4b4fe948a459d1b3a5c54f49
owbird <owbirdphorccuo@gmail.com>
2024-11-28 07:11:07 +0000

    add builder

---
+++ build.py
@@ -0,0 +1,16 @@
+from os.path import expanduser
+from subprocess import run
+
+user_path = expanduser("~")
+
+scripts = [
+    "apps",
+    "light",
+    "startup",
+    "workspace",
+    "workspace_completion"
+]
+
+for script in scripts:
+    run(["cp", f"{script}.py", f"{user_path}/Workspace/bin/"])
+    run(["chmod", "+x", f"{user_path}/Workspace/bin/{script}.py"])
