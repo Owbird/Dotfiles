eddd28fa612f84f8c7b6dfa28cada80737c4d23e
Owbird <40531477+Owbird@users.noreply.github.com>
2024-12-26 10:37:25 +0000

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -10,10 +10,13 @@ set -x PATH $FLYCTL_INSTALL/bin $PATH
 set -x PATH ~/Workspace/bin $PATH
 set -x PATH /opt/nvim-linux64/bin $PATH
 set -x PATH ~/.bun/bin $PATH
+set -x PATH ~/.local/share/fnm $PATH
+set -x PATH ~/.local/share/pnpm $PATH
 
 function fish_greeting
     fastfetch --logo ~/.config/fish/ascii.txt
     starship init fish | source
     #alias ls='exa -b --grid --icons -F -H --group-directories-first --git -1'
     alias ls='exa -b  --long --grid --icons --git'
+	fnm env --use-on-cd --shell fish | source
     end
