f83106148cbf0ff22c8f6c42824fac018bd76887
Owbird <owbirdphorccuo@gmail.com>
2024-12-26 12:08:15 +0000

    add config populator

---
+++ populate_config.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+CONFIG_DIR=~/.config 
+
+NVIM_CONFIG=$CONFIG_DIR/nvim
+FISH_CONFIG=$CONFIG_DIR/fish
+KITTY_CONFIG=$CONFIG_DIR/kitty
+STARSHIP_CONFIG=$CONFIG_DIR/starship.toml
+GIT_CONFIG=$CONFIG_DIR/.gitconfig
+
+
+rm -rf $NVIM_CONFIG
+cp -r neovim $NVIM_CONFIG
+
+rm -rf $FISH_CONFIG
+cp -r fish $FISH_CONFIG
+
+rm -rf $KITTY_CONFIG
+cp -r  kitty $KITTY_CONFIG
+
+cp git/gitconfig $GIT_CONFIG
+
+cp starship/starship.toml $STARSHIP_CONFIG
+
