2bd9dac24eda1fcdd0487e156e2693c540f4b0d9
Owbird <owbirdphorccuo@gmail.com>
2025-02-03 07:16:01 +0000

    add tmux config

--- populate_config.sh
+++ populate_config.sh
@@ -7,6 +7,8 @@ FISH_CONFIG=$CONFIG_DIR/fish
 KITTY_CONFIG=$CONFIG_DIR/kitty
 STARSHIP_CONFIG=$CONFIG_DIR/starship.toml
 GIT_CONFIG=~/.gitconfig
+TMUX_CONFIG=~/.tmux
+TMUX_POWERLINE_CONFIG=$CONFIG_DIR/tmux-powerline
 
 
 rm -rf $NVIM_CONFIG
@@ -17,6 +19,12 @@ cp -r fish $FISH_CONFIG
 
 rm -rf $KITTY_CONFIG
 cp -r  kitty $KITTY_CONFIG
+
+rm -rf $TMUX_CONFIG
+cp -r tmux $TMUX_CONFIG
+
+rm -rf $TMUX_POWERLINE_CONFIG
+cp -r tmux/tmux-powerline $TMUX_POWERLINE_CONFIG
 
 cp git/gitconfig $GIT_CONFIG
 
