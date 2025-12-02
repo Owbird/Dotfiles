805dd7098baeb634a190a5d48f9355309f3564ee
Owbird <owbirdphorccuo@gmail.com>
2025-12-02 17:26:17 +0000

    Update fish

--- fish/config.fish
+++ fish/config.fish
@@ -20,17 +20,11 @@ function fish_greeting
     starship init fish | source
     #alias ls='exa -b --grid --icons -F -H --group-directories-first --git -1'
     alias ls='exa -b  --long --grid --icons --git'
-	fnm env --use-on-cd --shell fish | source
+    alias ta='tmux a -t'
+    alias tl='tmux ls'
+    fnm env --use-on-cd --shell fish | source
     end
 
-function tses
-    if test (count $argv) -eq 0
-        echo "Usage: tses <session-name>"
-        return 1
-    end
-
-    tmux attach -t $argv[1] 2>/dev/null; or tmux new -s $argv[1]
-end
 
 
 set -gx PATH /usr/pgsql-17/bin $PATH
