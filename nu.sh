d7da14ce211c5ec77ff9398ea3a409338dd107eb
Owbird <owbirdphorccuo@gmail.com>
2026-02-27 20:41:03 +0000

    update nu

--- nu.sh
+++ nu.sh
@@ -1,5 +1,20 @@
+#!/usr/bin/bash
+
+
+format() {
+    awk 'NR==6 {print $8, $9}'
+}
+
+get_usage() {
+    vnstat -d 1 -i "$1"
+}
+
 while true; do
-clear
-vnstat -d 1 -i "wlan0+eth0" | sed -n '6p' | awk '{print $8,$9}'
-sleep 5
+    clear
+    echo $(date)
+    wifi_total=$(get_usage "wlan0" | format)
+    eth_total=$(get_usage "eth0" | format)
+    echo "WiFi: $wifi_total"
+    echo "Ethernet: $eth_total"
+    sleep 5
 done
