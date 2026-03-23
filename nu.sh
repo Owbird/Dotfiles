e422cc5bfe8d1268555d409a523c50ad96181e46
Owbird <owbirdphorccuo@gmail.com>
2026-03-23 20:58:39 +0000

    update nu

--- nu.sh
+++ nu.sh
@@ -12,9 +12,14 @@ get_usage() {
 while true; do
     clear
     echo $(date)
+    echo "-------------------"
     wifi_total=$(get_usage "wlan0" | format)
     eth_total=$(get_usage "eth0" | format)
+    total_usage=$(get_usage "wlan0+eth0" | format)
     echo "WiFi: $wifi_total"
     echo "Ethernet: $eth_total"
+    echo "******************"
+    echo "Total: $total_usage"
+    echo "******************"
     sleep 5
 done
